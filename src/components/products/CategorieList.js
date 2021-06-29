151a299f47bf9880492a25950cb00e83d165fcbb
lucasfranchini <lucasfranchini.a@gmail.com>
2021-06-29 11:32:43 -0300

    chaging titles and texts in categorilist

--- src/components/products/CategorieList.js
+++ src/components/products/CategorieList.js
@@ -1,13 +1,20 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import Categorie from "./Categorie";
 
 export default function CategorieList(){
+    const [categories,setCategories] = useState([]);
+    useEffect(()=>{
+
+    },[])
     return (
         <Body>
             <MainCategories>
-
+                <h1>Principais Categorias</h1>
+                {categories.map(c=><Categorie/>)}
             </MainCategories>
             <ShowCategories>
-                <button></button>
+                <button>Ver todas as categorias</button>
             </ShowCategories>
         </Body>
     );
@@ -17,6 +24,8 @@ const Body = styled.div`
     width: 100%;
     height: 308px;
     margin-top: 60px;
+    color: #FF4949;
+
 `
 
 const MainCategories = styled.div`
@@ -24,6 +33,11 @@ const MainCategories = styled.div`
     height: 267px;
     background-color: #fff;
     border-radius: 0px 0px 0px 67px;
+    h1{
+        text-align: center;
+        padding: 15px 0;
+        font-size: 24px;
+    }
 `
 const ShowCategories = styled.div`
     width: 100%;
@@ -34,6 +48,6 @@ const ShowCategories = styled.div`
             height: 100%;
             background-color: #fff;
             border: none;
-            border-radius: 0px 0px 67px 67px;;
+            border-radius: 0px 0px 67px 67px;
         }
 `

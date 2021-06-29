0127fb3f65977333e43cb5a6e5275f72c6bf3225
lucasfranchini <lucasfranchini.a@gmail.com>
2021-06-29 13:47:01 -0300

    defining layout for main Categorie, populating with a img and a name

--- src/components/products/MainCategorie.js
+++ src/components/products/MainCategorie.js
@@ -2,14 +2,37 @@ import styled from "styled-components";
 
 export default function MainCategorie({categorie,color}){
     return(
-        <Body color={color}>
-            <span>teste</span>
+        <Body >
+            <Figure color={color}>
+                <img src={categorie.img} alt={categorie.name}/>
+                
+            </Figure>
+            <span color={color}>{categorie.name}</span>
         </Body>
     );
 }
 const Body = styled.div`
     height: 189px;
     width: 156px;
-    color:${props=>props.color}
-
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    span{
+    color:${props=>props.color};
+    }
+    
+`
+const Figure = styled.div`
+    width: 156px;
+    height: 156px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img{
+        width: 70%;
+        height: 70%;
+    }
 `

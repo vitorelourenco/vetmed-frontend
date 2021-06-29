974061c04792f2635ce0508d416cb6fa302bd17c
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-29 16:42:30 -0300

    Add infinite background to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,29 @@ import logo39 from "../assets/images/vetmed-logo39.png";
 
 export default function Header(){
   return (
-    <HeaderWrapper>
-      <div className="pageheader--logobox">
-        <p className="logobox--text">vet<br/>med</p>
-        <img src={logo39} alt="VETMET LOGO" />
-      </div>
-      <div className="pageheader--menu">
+    <>
+      <HeaderBackground />
+      <HeaderWrapper>
+        <div className="pageheader--logobox">
+          <p className="logobox--text">vet<br/>med</p>
+          <img src={logo39} alt="VETMET LOGO" />
+        </div>
+        <div className="pageheader--menu">
 
-      </div>
-    </HeaderWrapper>
+        </div>
+      </HeaderWrapper>
+    </>
   );
 }
 
 const HeaderWrapper = styled.header`
   width: 100%;
   max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
   background-image: linear-gradient(90deg, white, white 50%, #FF4949 50%, #FF4949 100%);
   display: flex;
+  position: relative;
 
   .pageheader--logobox{
     flex: 0 0 auto;
@@ -27,6 +33,7 @@ const HeaderWrapper = styled.header`
     align-items: center;
     padding: 14px 10px 9px 10px;
     height: 62px;
+    z-index: 1;
 
     .logobox--text{
       font-size: 18px;
@@ -49,6 +56,7 @@ const HeaderWrapper = styled.header`
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
     padding: 8px;
+    z-index: 1;
 
     .menu--searchbar{
       flex: 1 1 100%;
@@ -57,3 +65,12 @@ const HeaderWrapper = styled.header`
 `;
 
 
+const HeaderBackground = styled.div`
+  background-image: linear-gradient(90deg, white, white 50%, #FF4949 50%, #FF4949 100%);
+  height: 62px;
+  width: 100vw;
+  z-index: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;

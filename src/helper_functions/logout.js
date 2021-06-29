2685abcece4516db0fa3943bd6f09782343058b7
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-29 15:31:10 -0300

    Remove unfinished feature from PR

--- src/helper_functions/logout.js
+++ src/helper_functions/logout.js
@@ -1,21 +1,8 @@
-import axios from "axios";
-
-import Config from "./Config";
-
 export default function logOut(user, setUser, history) {
   function clearAndPush() {
     localStorage.clear();
     setUser(null);
     history.push("/login");
   }
-
-  if (!user || !typeof user === "object" || !user.token) {
-    clearAndPush();
-  } else {
-    const config = new Config(user.token);
-    axios
-      .post("https://my-wallet-vel.herokuapp.com/logout", {}, config)
-      .catch((err) => alert(err))
-      .finally(() => clearAndPush());
-  }
+  clearAndPush();
 }

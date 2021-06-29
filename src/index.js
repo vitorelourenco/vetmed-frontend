45a7c0f6d08182be5d275a194f9b615ff01893ac
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-29 15:07:05 -0300

    Create user context and initial react-history

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

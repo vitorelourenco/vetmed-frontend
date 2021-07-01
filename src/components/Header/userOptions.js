91f18711ae67013fdc9cf47c4eef78f758726989
Vitor Lourenco <vitor.registros@lourencos.net>
2021-07-01 16:17:19 -0300

    Implement logout

--- src/components/Header/userOptions.js
+++ src/components/Header/userOptions.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link , useHistory} from "react-router-dom";
+import logOut from '../../helper_functions/logout';
+import UserContext from "../../contexts/UserContext";
+import { useContext } from "react";
 
 export function AuthedUserOptions(){
+  const history = useHistory();
+  const {user, setUser} = useContext(UserContext);
   return (
     <UserOptionsBox>
-      <Link to="/signup">Minhas compras</Link>
-      <Link to="/login">Sair</Link>
+      <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Minhas compras</Link>
+      <span onClick={()=>logOut(user, setUser, history)}>Sair</span>
     </UserOptionsBox>
   );
 }
@@ -28,7 +33,7 @@ const UserOptionsBox = styled.div`
     border-bottom: 1px solid white;
     padding-bottom: 2px;
   }
-  a{
+  a,span{
     width: 100%;
     text-align: center;
   }
